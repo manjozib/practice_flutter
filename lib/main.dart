b91e56846aa3b016048a7eee10fee185e8c99028
manjozib <manjozib@gmail.com>
2023-07-20 10:09:12 +0200

    changing from network image to asset image

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ void main() {
 }
 
 
+//PAGES
 //splash page
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -51,8 +52,6 @@ class SplashPage extends StatelessWidget {
     );
   }
 }
-
-
 
 //landing page after splash page
 class MountsApp extends StatelessWidget {
@@ -104,6 +103,8 @@ class MountsApp extends StatelessWidget {
   }
 }
 
+//Details page
+
 //WIDGETS
 
 
@@ -118,8 +119,8 @@ class AppHeader extends StatelessWidget {
       child: Row(
         children: [
           ClipOval(
-            child: Image.network(
-              'https://avatars.githubusercontent.com/u/5081804?v=4',
+            child: Image.asset(
+              'assets/images/img.png',
               width: 50,
               height: 50,
               fit: BoxFit.cover,
@@ -238,7 +239,7 @@ class AppMountListView extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               image: DecorationImage(
-                image: NetworkImage(currentMount.path),
+                image: AssetImage(currentMount.path),
                 fit: BoxFit.cover
               )
             ),
@@ -246,11 +247,13 @@ class AppMountListView extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  currentMount.name,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
+                Expanded(
+                  child: Text(
+                    currentMount.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
                 ),
                 Text(
@@ -473,35 +476,31 @@ class AppBottomBarItem {
 //DATA
 final List<MountModel> mountItems = [
   MountModel(
-      path:
-      'https://sa.kapamilya.com/absnews/abscbnnews/media/2021/afp/01/17/20210116-mt-semeru-indonesia-ash-afp-s.jpg',
-      name: 'Mount Semeru',
+      path: 'assets/images/semeru.jpeg',
+       name: 'Mount Semeru',
       description:
       'Semeru, or Mount Semeru, is an active volcano in East Java, Indonesia. It is located in the subduction zone, where the Indo-Australia plate subducts under the Eurasia plate. It is the highest mountain on the island of Java.',
       location: 'East Java'),
   MountModel(
-      path:
-      'https://media-cdn.tripadvisor.com/media/photo-s/04/a5/6f/ce/dsc-5622jpg.jpg',
+      path:'assets/images/merbaru.jpeg',
       name: 'Mount Merbaru',
       description:
       'Mount Merbabu is a dormant stratovolcano in Central Java province on the Indonesian island of Java. The name Merbabu could be loosely translated as Mountain of Ash from the Javanese combined words; Meru means mountain and awu or abu means ash.',
       location: 'Central Java'),
   MountModel(
-      path: 'https://cdn.dlmag.com/wp-content/uploads/2019/07/maunaloa1.jpg',
+      path: 'assets/images/loa.jpeg',
       name: 'Mauna Loa',
       description:
       'Mauna Loa is one of five volcanoes that form the Island of Hawaii in the U.S. state of Hawai in the Pacific Ocean. The largest subaerial volcano in both mass and volume, Mauna Loa has historically been considered the largest volcano on Earth, dwarfed only by Tamu Massif.',
       location: 'Hawaii'),
   MountModel(
-      path:
-      'https://cdn.images.express.co.uk/img/dynamic/78/590x/mount-vesuvius-1100807.jpg',
+      path:'assets/images/vesuvius.jpeg',
       name: 'Mount Vesuvius',
       description:
       'Mount Vesuvius is a somma-stratovolcano located on the Gulf of Naples in Campania, Italy, about 9 km east of Naples and a short distance from the shore. It is one of several volcanoes which form the Campanian volcanic arc.',
       location: 'Italy'),
   MountModel(
-      path:
-      'https://upload.wikimedia.org/wikipedia/commons/0/04/PopoAmeca2zoom.jpg',
+      path: 'assets/images/popocatépetl.jpeg',
       name: 'Mount Popocatépetl',
       description:
       'Popocatépetl is an active stratovolcano located in the states of Puebla, Morelos, and Mexico in central Mexico. It lies in the eastern half of the Trans-Mexican volcanic belt. At 5,426 m it is the second highest peak in Mexico, after Citlaltépetl at 5,636 m.',
