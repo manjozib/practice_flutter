68a63bb3bcdf89543cd74271d63c9dcb318bd355
manjozib <manjozib@gmail.com>
2023-07-21 15:22:55 +0200

    Developing rating bar in the bottom region of the details page

--- lib/main.dart
+++ lib/main.dart
@@ -205,7 +205,29 @@ class  DetailsPage extends StatelessWidget {
               ),
           ),
           Expanded(
-              child: Text('Upper')
+              child: Column(
+                children: [
+                  DetailsRatingBar()
+                  // Container(
+                  //   child: Row(
+                  //     children: [
+                  //       Container(
+                  //         child: Column(
+                  //           children: [
+                  //             Text(
+                  //               'Rating'
+                  //             ),
+                  //             Text(
+                  //                 '4.6'
+                  //             )
+                  //           ],
+                  //         ),
+                  //       )
+                  //     ],
+                  //   ),
+                  // )
+                ],
+              )
           )
         ],
       ),
@@ -458,7 +480,6 @@ class AppCategoryList extends StatelessWidget {
   }
 }
 
-
 //AppBottomBar
 class AppBottomBar extends StatefulWidget {
   const AppBottomBar({Key? key}) : super(key: key);
@@ -541,6 +562,60 @@ class _AppBottomBarState extends State<AppBottomBar> {
     );
   }
 }
+
+//DetailsRatingBar
+class DetailsRatingBar extends StatelessWidget {
+  //const DetailsRatingBar({Key? key}) : super(key: key);
+  
+  var sampleRatingData = {
+    'Rating': '4.6',
+    'Price': '\$12',
+    'Open': '24hrs'
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List.generate(
+            sampleRatingData.entries.length,
+                (index) => Container(
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey.withOpacity(0.2)
+                    ),
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        sampleRatingData.entries.elementAt(index).key,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        sampleRatingData.entries.elementAt(index).value,
+                        style: TextStyle(
+                          color: mainColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15
+                        ),
+                      )
+                    ],
+                  ),
+                )
+        ),
+      ),
+    );
+  }
+}
+
 
 
 
