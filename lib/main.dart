c14f8580a9b347b5a9bc36f6fef26dad343e3cb1
manjozib <manjozib@gmail.com>
2023-08-04 14:22:05 +0200

    creating gradient inside the container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 
+final Color mainYellow = Color(0xFFFFB02F);
+final Color primaryGray = Color(0xFF313131);
+final Color secondaryGray = Color(0xFF1C1C1C);
+final Color lightGray = Color(0xFF3B3B3B);
+
 void main() {
+  runApp(
+    MaterialApp(
+      home: LandingPage(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
 }
 
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              primaryGray,
+              secondaryGray
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter
+          )
+        ),
+      ),
+    );
+  }
+}
