b3c120f79d66cfe1f70c9db09f7b04cb66257bdf
manjozib <manjozib@gmail.com>
2023-08-04 15:53:07 +0200

    Populate the body of the Landing Page / Header

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,80 @@ class LandingPage extends StatelessWidget {
             end: Alignment.bottomCenter
           )
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            HeaderWidget(),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+//WIDGETS
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Where do you',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30
+                  ),
+                ),
+                Text(
+                  'want to go?',
+                  style: TextStyle(
+                      color: mainYellow,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(left: 30, right: 30),
+            child: Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: lightGray
+              ),
+              margin: EdgeInsets.only(top: 20, bottom: 20),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 10,),
+                  Text(
+                    'Search',
+                    style: TextStyle(
+                      color: Colors.grey
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+    );
+  }
+}
+
